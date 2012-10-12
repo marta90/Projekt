1c2acf46497abf5b976849e7baf487a0a5353199
Mateusz Jonak <mateusz.jonak@gmail.com>
2012-10-12 14:30:41 +0200

    Jest logowanie! Yoł!

--- serwis/serwis/urls.py
+++ serwis/serwis/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
+import os.path
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^serwis/$', 'serwis.zpi.views.glowna'),
+    url(r'^logowanie$', 'serwis.zpi.views.logowanie'),
     # Examples:
     # url(r'^$', 'serwis.views.home', name='home'),
     # url(r'^serwis/', include('serwis.foo.urls')),
@@ -14,4 +18,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^static2/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'static2')}),
 )
--- serwis/serwis/zpi/views.py
+++ serwis/serwis/zpi/views.py
@@ -1,1 +1,27 @@
+from django.shortcuts import render_to_response
+from serwis.zpi import models
+from django.http import HttpResponse, HttpResponseRedirect
+from passlib.hash import sha256_crypt
+
 # Create your views here.
+def glowna(request):
+    return render_to_response('index.html')
+
+def logowanie(request):
+    if request.method == 'POST':
+        nickPost = request.POST['fld_login']
+        hasloPost = request.POST['fld_pass']
+        models.Uzytkownik.objects
+        uz = models.Uzytkownik.objects.filter(nick=nickPost)
+        error = True
+        if uz.exists():
+            haslo = models.Uzytkownik.objects.get(nick=nickPost).haslo
+            true = sha256_crypt.verify(hasloPost, haslo)
+            #request.session['nick'] = nickPost # SESJA
+            #request.session['idUz'] = models.Uzytkownik.objects.get(nick=nickPost).id # SESJA
+            return HttpResponse(true)
+        else:
+            return HttpResponse('NIE OK')
+    #elif 'nick' in request.session:
+    #    return HttpResponseRedirect('stronaGlowna.html')
+    return render_to_response('logowanie.html')
