ab172d2e9cb75fdebac03f9bcda88efa5d54bff6
Marta Roman <martaemail@o2.pl>
2012-10-15 23:48:10 +0200

    Poprawione dla Barta

--- serwis/serwis/urls.py
+++ serwis/serwis/urls.py
@@ -18,5 +18,8 @@ urlpatterns = patterns('',
     (r'^media/html/calendar.htmll$', views.zaladujKalendarz),
     (r'^media/html/teachers.htmll$', views.zaladujWykladowcow), 
     (r'^media/html/map.html$', views.zaladujMape),
+    
+    (r'^css/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'media/html/css')}),
+    (r'^img/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'media/html/img')}),
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'media')}),
 )
